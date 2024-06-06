367bd44cf43f616a62d160c4107c0c235982614d
alex-f154 <a.ferreira1504@gmail.com>
2024-06-06 14:47:10 +0200

    deleted:    praktikumsplattform.db 	modified:   praktikumsplattform_script.sql

--- praktikumsplattform_script.sql
+++ praktikumsplattform_script.sql
@@ -1,24 +1,24 @@
-#CREATE database	praktikumsplattform;
-#USE praktikumsplattform;
+CREATE database	praktikumsplattform;
+USE praktikumsplattform;
 
 CREATE TABLE schueler
 	(schuelerid INT auto_increment PRIMARY KEY,
-    username_schueler VARCHAR(16),
+    username_schueler VARCHAR(30),
     password_schueler VARCHAR(16),
     wunschberuf VARCHAR(30),
     strasse VARCHAR(30),
-    Ort VARCHAR(30)
+    ort VARCHAR(30)
     );
 CREATE TABLE betriebe
 	(betriebid INT auto_increment PRIMARY KEY,
-    username_betrieb VARCHAR(16),
+    username_betrieb VARCHAR(30),
     password_betrieb VARCHAR(16),
     strasse VARCHAR(30),
-    Ort VARCHAR(30)
+    ort VARCHAR(30)
     );
 CREATE TABLE orte
 	(ortid INT auto_increment PRIMARY KEY,
-    ortname VARCHAR(255),
+    ortname VARCHAR(30),
     PLZ INT(5)
     );
     
